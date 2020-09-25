088964692d56a6fa0f0d96d6a792c75e2b503660
Andy Loftus <aloftus@gmail.com>
2020-09-25 10:30:36 -0500

    Reformat with simple structure, easier customization
    Add openstack ignores in default data.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,45 +1,88 @@
 # @summary Initial firewall configuration
 #
-# @param manage_builtin_chains
-#   If true (default), then pre-define built-in iptables chains.
-#   Set to false if another profile will manage built-in chains (e.g.
-#   profile_docker).
-#   See also: https://forge.puppet.com/puppetlabs/firewall
+# @param ignores
+#   Lists of ignore regex's for specific firewall chains.
+#   Keys must be in "CHAIN:TABLE:PROTOCOL" format.
+#   Values must be an Array of strings in Ruby regex format.
+#   See also:
+#   https://forge.puppet.com/puppetlabs/firewall/reference#firewallchain
 #
+# @param pre
+#   Exceptions to start the firewall rules
+#   Keys must begin with a 3-digit numer followed by a comment.
+#   The 3-digit number indicates firewall rule order, lower numbered rules are
+#   added before higher numbers.
+#   See also:
+#   https://forge.puppet.com/puppetlabs/firewall/readme#beginning-with-firewall
+#
+# @param post
+#   Exceptions to end the firewall rules.
+#   Keys must begin with a 3-digit numer followed by a comment.
+#   The 3-digit number indicates firewall rule order, lower numbered rules are
+#   added before higher numbers.
+#   See also:
+#   https://forge.puppet.com/puppetlabs/firewall/readme#beginning-with-firewall
+#
+# @param inbuilt_chains
+#   Default Linux chains. Module defaults should be sufficient.
+#   Keys must be in "CHAIN:TABLE:PROTOCOL" format.
+#   Values must be Hash of valid puppetlabs::firewallchain parameters
 # @example
 #   include profile_firewall
 class profile_firewall (
-  Boolean $manage_builtin_chains,
+  Hash $ignores,
+  Hash $inbuilt_chains,
+  Hash $post,
+  Hash $pre,
 ) {
 
   # DON'T EVER DO THIS ---
   # SEE: https://tickets.puppetlabs.com/browse/MODULES-5171
   # specifically..."using the resources resource will just purge 
   #                 all resources independent of the ignore statement"
-  # --- YOU HAVE BEEN WARNED
   # resources { 'firewall':
   #   purge =>  true,
   # }
   # resources { 'firewallchain':
   #   purge =>  true,
   # }
-
-  Firewall {
-    require =>  Class['profile_firewall::pre'],
-    before  => Class['profile_firewall::post'],
-  }
+  # --- YOU HAVE BEEN WARNED
 
   class { 'firewall':
     ebtables_manage => true,
   }
 
-  include ::profile_firewall::pre
-  include ::profile_firewall::post
-
-  #  notify { "manage_builtin_chains is ${$manage_builtin_chains}" : }
-  if $manage_builtin_chains {
-    # notify { 'about to include ::profile_firewall::builtin_chains' : }
-    include ::profile_firewall::builtin_chains
+  # CREATE DEFAULT CHAINS
+  $default_params = {
+    ensure => present,
+    purge  => true,
   }
+  $inbuilt_chains.each | $chain_name, $inbuilt_params | {
+
+    # Combine params from hiera with chain defaults
+    $custom_params = $inbuilt_params ? {
+      undef   => $default_params,
+      default => $default_params + $inbuilt_params,
+    }
+
+    # Add any ignores for this chain
+    $chain_params = $ignores[$chain_name] ? {
+      undef   => $custom_params,
+      default => $custom_params + { ignore => $ignores[$chain_name] },
+    }
+
+    # Debugging
+    # $chain_params.each | $key, $val | {
+    #   notify {"FWchain ${chain_name} ${key} ${val}" :}
+    # }
+
+    firewallchain { $chain_name :
+        * => $chain_params,
+    }
+
+  }
+
+  create_resources( firewall, $pre )
+  create_resources( firewall, $post )
 
 }
