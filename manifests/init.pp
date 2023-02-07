c7ef07b7d33d5939595cbbd94baaa838493a002f
Andy Loftus <aloftus@gmail.com>
2023-02-07 22:53:28 +0000

    add parameter ignore_chain_prefixes
    add custom fact custom_firewallchains
    remove parameter purge_all

--- manifests/init.pp
+++ manifests/init.pp
@@ -38,28 +38,34 @@
 #   Keys must be in "CHAIN:TABLE:PROTOCOL" format.
 #   Values must be Hash of valid puppetlabs::firewallchain parameters
 #
-# @param purge_all
-#   Used to control if Puppet will purge all unmanaged firewall rules and all unmanaged
-#   firewallchains.
-#   Default is true, if you plan on using $ignores you need to set this to false
-#   See https://tickets.puppetlabs.com/browse/MODULES-5171 (and README) for why this is needed
-#
+# # @param purge_all
+# #   DEPRECATED ???
+# #   Used to control if Puppet will purge all unmanaged firewall rules and all unmanaged
+# #   firewallchains.
+# #   Default is true, if you plan on using $ignores you need to set this to false
+# #   See https://tickets.puppetlabs.com/browse/MODULES-5171 (and README) for why this is needed
+
 # @example
 #   include profile_firewall
 class profile_firewall (
-  Hash $ignores,
-  Hash $inbuilt_chains,
-  Hash $post,
-  Hash $pre,
-  Boolean $purge_all,
-  Hash $rules,
+  Hash    $ignores,
+  Array   $ignore_chain_prefixes,
+  Hash    $inbuilt_chains,
+  Hash    $post,
+  Hash    $pre,
+  # Boolean $purge_all,
+  Hash    $rules,
 ) {
+
+  class { 'firewall':
+    ebtables_manage => true,
+  }
 
   # Check if we are using $ignores to decide if we should purge all unmanaged rules
   # SEE: https://tickets.puppetlabs.com/browse/MODULES-5171
   # specifically..."using the resources resource will just purge
   #                 all resources independent of the ignore statement"
-  if empty($ignores)  {
+  if empty($ignores) and empty($ignore_chain_prefixes) {
 
     # Not using ignores, ok to purge all
 
@@ -72,46 +78,56 @@ class profile_firewall (
 
   } else {
 
-    # If $ignores are being used, don't purge everything
+  #   # If $ignores are being used, don't purge everything
 
-    if $purge_all {
-      fail('$purge_all cannot be true when $ignores is not empty')
+  #   if $purge_all {
+  #     fail('$purge_all cannot be true when $ignores is not empty')
+  #   }
+  # }
+
+    # Ignore any non-standard chains matching specified prefixes
+    $ignore_chain_prefixes.each | $pfx | {
+      $facts['custom_firewallchains'].each | $chain, $data | {
+        if $chain.stdlib::start_with( $pfx ) {
+          notify { "IGNORE CHAIN ${chain}:${data['table']}:${data['protocol']}": }
+          firewallchain { "${chain}:${data['table']}:${data['protocol']}" :
+            purge => false,
+          }
+        }
+      }
+    }
+
+    # CREATE DEFAULT CHAINS
+    $default_params = {
+      ensure => present,
+      purge  => true,
+    }
+    $inbuilt_chains.each | $chain_name, $inbuilt_params | {
+
+      # Combine params from hiera with chain defaults
+      $custom_params = $inbuilt_params ? {
+        undef   => $default_params,
+        default => $default_params + $inbuilt_params,
+      }
+
+      # Add any ignores for this chain
+      $chain_params = $ignores[$chain_name] ? {
+        undef   => $custom_params,
+        default => $custom_params + { ignore => $ignores[$chain_name] },
+      }
+
+      # Debugging
+      # $chain_params.each | $key, $val | {
+      #   notify {"FWchain ${chain_name} ${key} ${val}" :}
+      # }
+
+      firewallchain { $chain_name :
+          * => $chain_params,
+      }
+
     }
   }
 
-  class { 'firewall':
-    ebtables_manage => true,
-  }
-
-  # CREATE DEFAULT CHAINS
-  $default_params = {
-    ensure => present,
-    purge  => true,
-  }
-  $inbuilt_chains.each | $chain_name, $inbuilt_params | {
-
-    # Combine params from hiera with chain defaults
-    $custom_params = $inbuilt_params ? {
-      undef   => $default_params,
-      default => $default_params + $inbuilt_params,
-    }
-
-    # Add any ignores for this chain
-    $chain_params = $ignores[$chain_name] ? {
-      undef   => $custom_params,
-      default => $custom_params + { ignore => $ignores[$chain_name] },
-    }
-
-    # Debugging
-    # $chain_params.each | $key, $val | {
-    #   notify {"FWchain ${chain_name} ${key} ${val}" :}
-    # }
-
-    firewallchain { $chain_name :
-        * => $chain_params,
-    }
-
-  }
 
   create_resources( firewall, $pre )
   if ( $rules ) {
